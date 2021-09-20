9bb408b77f6070ab5fa9ddf14154ca66e7c9b32b
rubens <rubens@DESKTOP-8I7T2LQ>
2021-09-20 10:35:52 -0300

    nuevo archivo py.

---
+++ test1.py
@@ -0,0 +1,15 @@
+print("Hola Mundo!\n")
+List1 = ["hola","mundo","!"]
+print(List1)
+
+# List2 = List1  #referencia la lista, no es una copia
+List2 = List1[:]  # clona la lista, es una copia
+
+List1[0] = "Hello"
+List1[1] = "world"
+# imprime nueva lista
+
+print("Lista 1: ", List1)
+print("Lista 2: ", List2)
+
+print("Fin del programa!")
